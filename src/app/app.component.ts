8d68393b705c5f4a3ca6ed186686cc0486a61876
root <yuriy.chaban@masterofcode.com>
2017-12-28 17:13:44 +0200

    add simple project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+
+	@Input()
+	count = 0;
+
+	@Output()
+	change: EventEmitter<number> = new EventEmitter<number>();
+
+	increment() {
+		this.count++;
+		this.change.emit(this.count);
+	}
+
+	decrement() {
+		this.count--;
+		this.change.emit(this.count);
+
+		if (this.count < 0) {
+			this.count = 0;
+			alert("Number cannot be less than zero!");
+		}
+	}
+
 }
